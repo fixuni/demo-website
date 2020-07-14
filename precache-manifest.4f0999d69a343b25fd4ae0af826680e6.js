fe1eaada0ff2994ccaa200c80e2de852521a4a0d
Minho.Yoo <paromix@gmail.com>
2020-07-14 17:06:34 +0900

    Updates

--- precache-manifest.4f0999d69a343b25fd4ae0af826680e6.js
+++ precache-manifest.4f0999d69a343b25fd4ae0af826680e6.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/demo-website/static/media/uport-app-icon.3be74989.svg"
   },
   {
-    "revision": "66a8107a7d5aabb59e52",
+    "revision": "429d73ea1d7a027b542a",
     "url": "/demo-website/static/css/main.b0b63eda.chunk.css"
   },
   {
@@ -60,8 +60,8 @@ self.__precacheManifest = [
     "url": "/demo-website/static/media/city-id-icon.03830ca9.svg"
   },
   {
-    "revision": "66a8107a7d5aabb59e52",
-    "url": "/demo-website/static/js/main.3f7dd887.chunk.js"
+    "revision": "429d73ea1d7a027b542a",
+    "url": "/demo-website/static/js/main.caf6b1e7.chunk.js"
   },
   {
     "revision": "5f54095e178a888b118a75d9bebac876",
@@ -116,7 +116,7 @@ self.__precacheManifest = [
     "url": "/demo-website/static/media/smiley-face-diploma.12b3afd3.svg"
   },
   {
-    "revision": "64d89c605f2dd9d78e202ce937ee291f",
+    "revision": "d6988c4aba32b2ebcbaf661dd049808e",
     "url": "/demo-website/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/demo-website/precache-manifest.47404c95a3f9644b9f9b64064f6ed23e.js"
+  "/demo-website/precache-manifest.4f0999d69a343b25fd4ae0af826680e6.js"
 );
 
 workbox.clientsClaim();
